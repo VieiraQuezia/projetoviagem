d1b56f25b031cc7f0b6c491178418dfe780f0dd4
Hayeska <loredanehayeska@gmail.com>
2025-03-06 15:52:30 -0300

    alt

--- src/Pages/NavBar/Carros.jsx
+++ src/Pages/NavBar/Carros.jsx
@@ -1,18 +1,33 @@
-import Card from "../../Components/card"
+import Header from "../../Components/header.jsx";
+import Footer from "../../Components/footer.jsx";
+import NavBar from "../../Components/navBar.jsx";
+import Instagram from "/instagram.png";
+import Gmail from "/gmail.png";
+import Card from "../../Components/card.jsx";
 import Carro from "/carro.png"
-import "/App.css"
+import "../../App.css"
 
 
-function Carros () {
+function Carros() {
     return (
         <div>
-        <a href="/carro1"><Card img={Carro} viagem="Carro Econômico" saida="Em Lisboa" info="Com Budget" valor="R$ 148"/></a>
+            <Header />
 
-        <a href="/carro2"><Card img={Carro} viagem="Carro Econômico" saida="Em Santiago" info="Com Hertz" valor="R$ 130"/></a>
+            <NavBar />
+            <Card img={Carro} viagem="Carro Econômico" saida="Em Lisboa" info="Com Budget" valor="R$ 148" />
 
-        <a href="/carro3"><Card img={Carro} viagem="Carro Econômico" saida="Em Belo Horizonte" info="Com Movida" valor="R$ 135"/></a>
+            <Card img={Carro} viagem="Carro Econômico" saida="Em Santiago" info="Com Hertz" valor="R$ 130" />
 
-        <a href="/carro4"><Card img={Carro} viagem="Carro Econômico" saida="Em Miami" info="Com Florida Premium RentACar" valor="R$ 244"/></a>
+            <Card img={Carro} viagem="Carro Econômico" saida="Em Belo Horizonte" info="Com Movida" valor="R$ 135" />
+
+            <Card img={Carro} viagem="Carro Econômico" saida="Em Miami" info="Com Florida Premium RentACar" valor="R$ 244" />
+            <Footer
+                imgi={Instagram}
+                insta="@grupo6"
+                imge={Gmail}
+                email="grupo6@gmail.com"
+                texto="Responsável pelo desenvolvimento: Grupo 06"
+            />
         </div>
     )
 }
