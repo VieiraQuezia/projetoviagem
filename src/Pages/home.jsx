6b59d3d53805fbcbf53630ab54c92721692a3da2
Guih <ggsantana18@gmail.com>
2025-03-12 08:51:42 -0300

    att

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,31 +1,67 @@
 import Header from "../components/header.jsx";
 import Footer from "../components/Footer";
 import NavBar from "../components/navBar.jsx";
-import Slider from '../Components/slider.jsx'
+import Slider from "../Components/slider.jsx";
 import Instagram from "/instagram.png";
 import gmail from "/gmail.png";
-import Card from '../Components/card.jsx'
-import Japao from '/japao.png'
-import Mexico from '/cidade-do-mexico.jpg'
-import Roma from '/Roma.jpg'
-import Brasil from '/brasil.jpg'
+import Card from "../Components/card.jsx";
+import Japao from "/japao.jpg";
+import Mexico from "/mexico.jpg";
+import Roma from "/Roma.jpg";
+import Brasil from "/Brasil.png";
 import "../App.css";
 
 function App() {
   return (
     <>
       <Header />
-<Slider/>      <NavBar />
+      <Slider /> 
+      <NavBar />
       <div className="blocos">
-      <a href="/italia"><Card img={Roma} avaliacao="⭐⭐⭐⭐⭐" viagem="Pacotes para a Itália" saida="Saindo de São Paulo" info="Preço final por pessoa" valor="R$3.993,00"/></a>
-      
-      <a href="/japao"><Card img={Japao} avaliacao="⭐⭐⭐⭐⭐" viagem="Pacotes para o Japão" saida="Saindo de São Paulo" info="Preço final por pessoa" valor="R$3.993,00"/></a>
+        <a href="/italia">
+          <Card
+            imge={Roma}
+            avaliacao="⭐⭐⭐⭐⭐"
+            viagem="Pacotes para a Itália"
+            saida="Saindo de São Paulo"
+            info="Preço final por pessoa"
+            valor="R$3.993,00"
+          />
+        </a>
 
-      <a href="/mexico"><Card img={Mexico} avaliacao="⭐⭐⭐⭐⭐" viagem="Pacotes para o México" saida="Saindo de São Paulo" info="Preço final por pessoa" valor="R$3.993,00"/></a>
+        <a href="/japao">
+          <Card
+            imge={Japao}
+            avaliacao="⭐⭐⭐⭐⭐"
+            viagem="Pacotes para o Japão"
+            saida="Saindo de São Paulo"
+            info="Preço final por pessoa"
+            valor="R$3.993,00"
+          />
+        </a>
 
-      <a href="/brasil"><Card img={Brasil} avaliacao="⭐⭐⭐⭐⭐" viagem="Pacotes para o Brasil" saida="Saindo de São Paulo" info="Preço final por pessoa" valor="R$3.993,00"/></a>
+        <a href="/mexico">
+          <Card
+            imge={Mexico}
+            avaliacao="⭐⭐⭐⭐⭐"
+            viagem="Pacotes para o México"
+            saida="Saindo de São Paulo"
+            info="Preço final por pessoa"
+            valor="R$3.993,00"
+          />
+        </a>
+
+        <a href="/brasil">
+          <Card
+            imge={Brasil}
+            avaliacao="⭐⭐⭐⭐⭐"
+            viagem="Pacotes para o Brasil"
+            saida="Saindo de São Paulo"
+            info="Preço final por pessoa"
+            valor="R$3.993,00"
+          />
+        </a>
       </div>
-
       <Footer
         imgi={Instagram}
         insta="@grupo6"
@@ -38,8 +74,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-   
