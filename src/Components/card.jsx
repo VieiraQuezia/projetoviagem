32245e3ee99dfa857d934bcbefaad11cac4152ba
Quezia Vieira <vieiraquezia24@gmail.com>
2025-03-12 07:29:57 -0300

    alt

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,21 +1,19 @@
-import "./card.css";
+  import "./card.css";
 
-function card(props) {
+  function card(props) {
 
-  return (
-    <>
-      <div className="blocos">
-        <nav className="bloco">
-          <img className="imagem" src={props.img} />
-          <h1>{props.viagem}</h1>
-          <h3>{props.avaliacao}</h3>
-          <p>{props.saida}</p>
-          <p>{props.info}</p>
-          <h2>{props.valor}</h2>
-        </nav>
-      </div>
-    </>
-  );
-}
+    return (
+      <>
+          <nav className="bloco">
+            <img className="imagem" src={props.img} />
+            <h1>{props.viagem}</h1>
+            <h3>{props.avaliacao}</h3>
+            <p>{props.saida}</p>
+            <p>{props.info}</p>
+            <h2>{props.valor}</h2>
+          </nav>
+      </>
+    );
+  }
 
-export default card;
+  export default card;
